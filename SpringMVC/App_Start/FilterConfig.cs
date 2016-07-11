0b638808b1509706ebf4e38eaa6a3592ab071fbc
RYJ <2636922684@qq.com>
2016-07-11 16:28:13 +0800

    Spring.Net简单演示
    IOC之Spring.Net简单演示

---
+++ SpringMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SpringMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SpringMVC/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpringMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public string AdminService { get; set; }
+        public ActionResult Index()
+        {
+            return Content(AdminService);
+        }
+
+    }
+}
